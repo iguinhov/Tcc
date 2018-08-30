00f10ca30f83faa9266242f426e5ec14c5b00b36
Igor Carreiro <igor.carreiro@gmail.com>
2018-08-30 17:40:23 -0300

    Inclusão da classe de usarios e view Index

---
+++ Tcc/Models/Usuarios.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Tcc.Models
+{
+    public class Usuarios
+    {
+        public int ID { get; set; }
+
+        
+        [Required]
+        public string Password { get; set; }
+
+    }
+}
